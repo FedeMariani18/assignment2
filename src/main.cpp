b26df062af0fa915d68e97a88425f33b5b800dd1
Federico Mariani <federico.mariani3@studio.unibo.it>
2025-11-27 19:29:48 +0100

    added context to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 #include "kernel/Scheduler.h"
+#include "model/Context.h"
+#include "model/ContextAlarm.h"
 
 Scheduler sched;
+Context context;
+ContextAlarm contextAlarm;
 
 void setup() {
 
