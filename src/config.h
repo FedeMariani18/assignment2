fc2ea6cbd2a0be11dceeda4682f27e5d375037ee
Federico Mariani <federico.mariani3@studio.unibo.it>
2025-11-27 16:30:26 +0100

    added kernel implementation and model implementation and the file config

---
+++ src/config.h
@@ -0,0 +1,12 @@
+const int BT_PIN = 0;
+const int LED_PIN = 0;
+const int MOTOR_PIN = 0;
+const int PIR_PIN = 0;
+const int SONAR_ECHO_PIN = 0;
+const int SONAR_TRIG_PIN = 0;
+const int SONAR_TIMEOUT = 0;
+const int TEMP_PIN = 0;
+
+const int LCD_ADRR = 0;
+const int LCD_COLS = 0;
+const int LCD_ROWS = 0;
